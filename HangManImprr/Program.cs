007a53912116ea0f9e8607f1d086a60318b22f1e
zglina <ddominikzglinski@gmail.com>
2021-07-12 01:21:12 +0200

    First Code Push

--- HangManImprr/Program.cs
+++ HangManImprr/Program.cs
@@ -9,11 +9,32 @@ namespace HangManImpr
         {
             Console.WriteLine("Program Start");
 
+            int play;
             string[] file = System.IO.File.ReadAllLines(@".\countries_and_capitals.txt"); //otwieram plik tekstowy z miastami i  stolicami
 
 
             Console.WriteLine(file[0]);
             RandomLineGenerator(file);
+
+            Console.WriteLine("Do you want to play the game? If yes press '1', if not, press anything else");
+            play = Convert.ToInt32(Console.Read());
+
+            Console.WriteLine(play);
+            Console.ReadKey();
+
+
+            while(play==1)
+            {
+
+
+
+                Console.WriteLine("You are in the loop");
+                Console.ReadKey();
+
+
+            }
+            
+
 
 
 
